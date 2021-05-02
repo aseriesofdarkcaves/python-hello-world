1470f90d64f5d773bec65bbe0319a39cafcca35e
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2021-05-02 23:26:00 +0200

    add hello-loops.py

---
+++ hello-loops.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+phrases = [
+    "Nobody expects the Spanish Inquisition!",
+    "'Tis but a scratch!",
+    "And now for something completely different...",
+    "My hovercraft is full of eels",
+    "I fart in your general direction!",
+    "She's got huge... tracts of land"
+]
+
+for phrase in phrases:
+    sleep(2)
+    print(phrase)
