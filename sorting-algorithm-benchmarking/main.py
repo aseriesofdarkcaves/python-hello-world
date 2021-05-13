e5c2a2e1418e9960b198277e395aade4294891a0
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2021-05-13 23:05:48 +0200

    Update to benchmarking - use a dictionary instead of a list

--- sorting-algorithm-benchmarking/main.py
+++ sorting-algorithm-benchmarking/main.py
@@ -30,9 +30,8 @@ def get_unsorted_lists():
 
 if __name__ == "__main__":
     """Do the benchmarking of the sorting algorithms"""
-    # use a dictionary with size as key?
-    averaged_results = []
-    # averaged_results = {}
+    # TODO: figure out a way to iterate over the algorithms
+    averaged_results = {}
 
     for unsorted_list in get_unsorted_lists():
         single_cumulative_result = 0
@@ -42,8 +41,6 @@ if __name__ == "__main__":
             end_time = time.time()
             elapsed_time = end_time - start_time
             single_cumulative_result += elapsed_time
-        averaged_results.append(round(single_cumulative_result / 10, 3))
-        # averaged_results[len(unsorted_list)] = (single_cumulative_result / 10)
+        averaged_results[len(unsorted_list)] = round((single_cumulative_result / 10), 3)
 
-    for result in averaged_results:
-        print(result)
+    print(averaged_results)
