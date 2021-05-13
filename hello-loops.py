60fcbedd5a511d649c533e3fb2700a1e7d0b1e27
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2021-05-13 10:12:17 +0200

    Hello Loops - add another interesting loop - refactor to use functions - add Python Docstrings to functions

--- hello-loops.py
+++ hello-loops.py
@@ -1,14 +1,41 @@
-from time import sleep
+def print_phrases():
+    """Print Monty-Python phrases to the console"""
+    phrases = [
+        "Nobody expects the Spanish Inquisition!",
+        "'Tis but a scratch!",
+        "And now for something completely different...",
+        "My hovercraft is full of eels",
+        "I fart in your general direction!",
+        "She's got huge... tracts of land"
+    ]
 
-phrases = [
-    "Nobody expects the Spanish Inquisition!",
-    "'Tis but a scratch!",
-    "And now for something completely different...",
-    "My hovercraft is full of eels",
-    "I fart in your general direction!",
-    "She's got huge... tracts of land"
-]
+    for phrase in phrases:
+        print(phrase)
 
-for phrase in phrases:
-    sleep(2)
-    print(phrase)
+
+def print_users():
+    """Print user dictionary to the console, before and after manipulation by a for loop"""
+    users = {
+        "gchapman": "inactive",
+        "jcleese": "active",
+        "tgilliam": "active",
+        "eidle": "active",
+        "tjones": "inactive",
+        "mpalin": "active"
+    }
+
+    # initial implementation of concatenation in print function:
+    # print("BEFORE: " + users.__str__())
+    # but Python can do this - and it reads much better
+    print("BEFORE: ", users)
+
+    # copy the dictionary for iteration, but make changes to the original
+    for user, status in users.copy().items():
+        if status == "inactive":
+            del users[user]
+
+    print("AFTER: ", users)
+
+
+print_phrases()
+print_users()
