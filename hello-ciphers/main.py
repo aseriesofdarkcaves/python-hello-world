96b060e1826e0d91a73067e9b85465492a7d0b5a
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2023-02-20 14:49:50 +0100

    Add initial hello-ciphers implementation

---
+++ hello-ciphers/main.py
@@ -0,0 +1,60 @@
+import string
+
+
+def init_ciphermap(key_list, value_list):
+    """
+    Initialise a dictionary with the desired mapping.
+    :param key_list: the list of keys to define
+    :param value_list: the list of values to which to map the keys
+    :return: a dictionary with the desired mapping
+    """
+    return dict(zip(key_list, value_list))
+
+
+def print_plaintext(ciphertext, ciphermap):
+    """
+    Print the plaintext version of the ciphertext using the given ciphermap.
+    :param ciphertext: the ciphertext which should be iterated over
+    :param ciphermap: the dictionary to use to map the ciphertext to plaintext
+    :return: None
+    """
+    print('#################### OUTPUT ####################')
+    for entry in ciphertext:
+        if entry.isnumeric():
+            key = int(entry) % 26
+            if key in cipermap:
+                print(cipermap[key], end=' ')
+            else:
+                print('?', end=' ')
+        else:
+            print(entry, end=' ')
+
+
+if __name__ == "__main__":
+    ciphertext = [
+        '9', '1', '7', '68',
+        ' ',
+        '51', '2', '8', '11',
+        ' ',
+        '7', '68',
+        ' ',
+        '19', '23', '8', '9', '1',
+        ' ',
+        '2',
+        ' ',
+        '9', '7', '51', '87', '18', '9',
+        ' ',
+        '9', '23',
+        ' ',
+        '2',
+        ' ',
+        '18', '68', '51', '2', '3', '18',
+        ' ',
+        '8', '23', '23', '14',
+        ' ',
+        '56', '2', '14', '18'
+    ]
+
+    cipermap = init_ciphermap(list(range(0, 26)), string.ascii_lowercase)
+
+    print_plaintext(ciphertext, cipermap)
